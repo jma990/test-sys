83b32590796486fd9b46e75133e1ca08e38460a2
Makehaumea <127465191+jma990@users.noreply.github.com>
2025-10-15 08:10:20 +0000

    Announcements Frontend

--- Content-Management-System/obj/Debug/net8.0/Content-Management-System.AssemblyInfo.cs
+++ Content-Management-System/obj/Debug/net8.0/Content-Management-System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Content-Management-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d9959009f4894744c308373f65adbb966c3df2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366b8dfc6512ab576f4871bb8def012d2d2b1ebd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Content-Management-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Content-Management-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
