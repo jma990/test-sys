50038447c67eff7b1aa8df88444d055d8dd8fcac
Makehaumea <127465191+jma990@users.noreply.github.com>
2025-11-05 06:26:55 +0000

    Bug fixes before trying to host

--- Content-Management-System/obj/Debug/net8.0/Content-Management-System.AssemblyInfo.cs
+++ Content-Management-System/obj/Debug/net8.0/Content-Management-System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Content-Management-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ec581fbca7233783b46570abfd961cce1a5b50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b04619db04b1cd348d71dbe7fc03455adfb3d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Content-Management-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Content-Management-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
